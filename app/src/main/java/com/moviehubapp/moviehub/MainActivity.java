03e4391ca7325614ff4abce446d335096a1c2f4b
TeeJay Andrews <TeeJay3Andrews@gmail.com>
2016-08-27 16:25:29 -0400

    Add Scrolling Animation To Adapter.

--- app/src/main/java/com/moviehubapp/moviehub/MainActivity.java
+++ app/src/main/java/com/moviehubapp/moviehub/MainActivity.java
@@ -12,6 +12,25 @@ import android.view.MenuItem;
 
 import java.util.List;
 
+import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter;
+
+/**   Copyright 2015 Wasabeef
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ *    Origin https://github.com/wasabeef/recyclerview-animators
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     private final String LOG_TAG = MainActivity.class.getSimpleName();
@@ -33,7 +52,11 @@ public class MainActivity extends AppCompatActivity {
                 RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
                 Log.v(LOG_TAG, "Creating Adapter.");
                 ImageAdapter imageAdapter = new ImageAdapter(context, result);
-                recyclerView.setAdapter(imageAdapter);
+
+                ScaleInAnimationAdapter scaleInAnimationAdapter =
+                        new ScaleInAnimationAdapter(imageAdapter);
+                scaleInAnimationAdapter.setFirstOnly(false);
+                recyclerView.setAdapter(scaleInAnimationAdapter);
 
                 GridLayoutManager gridLayout =
                         new GridLayoutManager(context, 2);
